f2e15753e472a0f636a925b54348c51cfbee2f24
acsmdev <acsmdev@gmail.com>
2022-05-10 23:32:01 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -14,7 +14,7 @@ export function Pagination() {
                 <button className="dsmovie-pagination-button" disabled={false} >
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
-            </div>
+            </div>            
         </div>
 
     );
